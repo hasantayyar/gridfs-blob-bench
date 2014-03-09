9b636a35cd01e65846d86e689ee43fca0db20dec
hasantayyar <tayyar.besik@gmail.com>
2014-03-09 21:26:36 +0200

    test files

---
+++ test100.sh
@@ -0,0 +1,5 @@
+echo "####testing mysql";
+php src/run.php get_mysql.php 100
+
+echo "####testing mongodb";
+php src/run.php get_mongo.php 100
---
+++ test1000.sh
@@ -0,0 +1,5 @@
+echo "####testing mysql";
+php run.php get_mysql.php 1000
+
+echo "####testing mongodb";
+php run.php get_mongo.php 1000
